8df3e2d111c4b13c843d831fc43d262119e7ae03
James Barron <jamesbarron97@gmail.com>
2020-11-27 23:12:20 +0000

    yet more stuff

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,42 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import classNames from "classnames";
 
-import { bombsBoard, emptyBoard, fullBoard } from "../helpers";
 import Cell from "./Cell";
 
 const Board = (props) => {
 	const classes = classNames("board-root", props.difficulty);
-	const [rows, cols, bombCount] = [props.rows, props.cols, props.bombCount];
-	const [board, setBoard] = useState(emptyBoard(rows, cols));
 
 	const startGame = (row, col) => {
-		setBoard(
-			fullBoard(bombsBoard(emptyBoard(rows, cols), bombCount, row, col))
-		);
-		props.startGame();
+		props.startGame(row, col);
 	};
 
 	return (
-		<div className={classes}>
-			{board.map((row, rowIdx) => {
-				return row.map((cell, colIdx) => {
-					return (
-						<Cell
-							key={colIdx}
-							row={rowIdx}
-							col={colIdx}
-							value={cell}
-							endGame={props.endGame}
-							gameState={props.gameState}
-							startGame={startGame}
-							handleCellCount={props.handleCellCount}
-							handleBombCount={props.handleBombCount}
-							bombsLeft={props.bombsLeft}
-						/>
-					);
-				});
-			})}
-		</div>
+		<>
+			<div className={classes}>
+				{props.board.map((row, rowIdx) => {
+					return row.map((cell, colIdx) => {
+						return (
+							<Cell
+								key={colIdx}
+								row={rowIdx}
+								col={colIdx}
+								value={cell}
+								endGame={props.endGame}
+								gameState={props.gameState}
+								startGame={startGame}
+								handleCellCount={props.handleCellCount}
+								handleBombCount={props.handleBombCount}
+								bombsLeft={props.bombsLeft}
+							/>
+						);
+					});
+				})}
+			</div>
+		</>
 	);
 };
 
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,11 +3,14 @@ import React, { useState, useEffect } from "react";
 import GameInfo from "./GameInfo";
 import Board from "./Board";
 import gameStatus from "../gameStatus.json";
-import { difficulties } from "../helpers";
+import { difficulties, emptyBoard, bombsBoard, fullBoard } from "../helpers";
 
 const Game = () => {
 	const [gameState, setGameState] = useState(gameStatus.NOT_STARTED);
 	const [difficulty, setDifficulty] = useState(difficulties("easy"));
+	const [board, setBoard] = useState(
+		emptyBoard(difficulty.rows, difficulty.cols)
+	);
 	const [bombCount, setBombCount] = useState(difficulty.bombs);
 	const [cellsToOpen, setCellsToOpen] = useState(
 		difficulty.rows * difficulty.cols - difficulty.bombs
@@ -20,8 +23,30 @@ const Game = () => {
 		}
 	}, [cellsToOpen]);
 
-	const startGame = () => setGameState(gameStatus.IN_PROGRESS);
-	const endGame = () => setGameState(gameStatus.LOST);
+	const startGame = (row, col) => {
+		setBoard(
+			fullBoard(
+				bombsBoard(
+					emptyBoard(difficulty.rows, difficulty.cols),
+					difficulty.bombs,
+					row,
+					col
+				)
+			)
+		);
+		setGameState(gameStatus.IN_PROGRESS);
+	};
+
+	const endGame = () => {
+		setGameState(gameStatus.LOST);
+	};
+	const newGame = (e) => {
+		e.preventDefault();
+		const diff = difficulties(e.target.name);
+		setGameState(gameStatus.NOT_STARTED);
+		setBoard(emptyBoard(diff.rows, diff.cols));
+		setDifficulty(diff);
+	};
 
 	const handleCellCount = () => {
 		setCellsToOpen((cellsToOpen) => cellsToOpen - 1);
@@ -41,6 +66,7 @@ const Game = () => {
 				setTime={(time) => setTime(time)}
 			/>
 			<Board
+				board={board}
 				difficulty={difficulty.name}
 				rows={difficulty.rows}
 				cols={difficulty.cols}
@@ -54,6 +80,16 @@ const Game = () => {
 			/>
 			{gameState === gameStatus.LOST ? <div>YOU LOSE</div> : null}
 			{gameState === gameStatus.WON ? <div>YOU WIN</div> : null}
+			<br />
+			<button name='easy' onClick={newGame}>
+				New Easy Game
+			</button>
+			<button name='medium' onClick={newGame}>
+				New Medium Game
+			</button>
+			<button name='hard' onClick={newGame}>
+				New Hard Game
+			</button>
 		</>
 	);
 };
